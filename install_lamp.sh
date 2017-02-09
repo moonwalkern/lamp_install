931bec7b3deaffca8a6195171ca01fc3f4c0bf8c
Your Name <you@example.com>
2017-02-09 15:38:08 -0800

    adding lamp installer

---
+++ install_lamp.sh
@@ -0,0 +1,6 @@
+sudo apt-get install apache2
+sudo apt-get install mysql-server
+sudo apt install  php7.0-cli
+php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
+sudo /etc/init.d/apache2 restart
+
