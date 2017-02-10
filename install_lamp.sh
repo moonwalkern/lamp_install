5a56879d85c7bc0824e419a1acefd3b0d0916448
Your Name <you@example.com>
2017-02-09 16:14:13 -0800

    updated

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,6 +1,7 @@
 sudo apt-get install apache2
 sudo apt-get install mysql-server
 sudo apt install  php7.0-cli
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
 php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
 sudo /etc/init.d/apache2 restart
 
