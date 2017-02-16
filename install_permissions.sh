003605a467330df597f859fd3a13ab231bbcbadb
Your Name <you@example.com>
2017-02-16 06:26:01 -0800

    updating

--- install_permissions.sh
+++ install_permissions.sh
@@ -1,7 +1,8 @@
 echo "Creating Premission"
 sudo groupadd www
 sudo usermod -a -G www $USER
-sudo chown -R root:www /var/www
-sudo chmod 2775 /var/www
+sudo chown -R root:www /var/www/
+sudo chmod 2775 /var/www/*
 find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
